b0a32e785d1b92c275f01bf665e5ec1e4aba7f00
Codingczp <weichengchikuang@sina.cn>
2019-02-28 21:36:57 +0800

    basical grammer

---
+++ basicalGrammer.py
@@ -0,0 +1,16 @@
+# some methods to translate data type
+int(x)
+float(x)
+complex(real)
+str(x)
+repr(x)
+eval(str)
+tuple(s)
+list(s)
+set(s)
+dict(d)
+frozenset(s)    #make a set unchangeable
+chr(x)
+ord(x)  #make a char to int
+hex(x)  #make a number to sixten formate
+oct(x)  
